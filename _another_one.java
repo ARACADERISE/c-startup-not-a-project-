fedadb86e73210612d68ff656daca94f49f3df92
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-23 12:33:30 -0500

    Update _another_one.java

--- _another_one.java
+++ _another_one.java
@@ -1,5 +1,10 @@
-import
+import pract.pract;
 
-public class _another_one {
-
+public class _another_one extends pract {
+  public _another_one() {
+    NAME = "Aidan";
+  }
+  public static void main(String[] args) {
+    _another_one name_is = new _another_one();
+  }
 }
