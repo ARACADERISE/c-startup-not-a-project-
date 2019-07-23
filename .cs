439c75be3ea629e6a25d7dba8e3d6385707928d8
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-22 21:07:01 -0500

    Create .cs

---
+++ .cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomethingToDoWithCeeSharp
+{
+  class AClass
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("You just opened this file for no reason cause this is useless right now");
+    }
+  }
+}
