8a29a20e32db4b6e300b6cf44ace4be89aff9ece
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-23 12:30:09 -0500

    Update pract.java

--- pract.java
+++ pract.java
@@ -1,3 +1,14 @@
-public class pract {
+/*
+* I do not know what exactly it is that I am doing,
+* but I guess I am just trying to understand Java more.
+*/
 
+// I am now going to make another file that will extend this class
+// of which will then return my name
+public class pract {
+  protected static String NAME;
+  
+  public String returnName() {
+    return this.NAME
+  }
 }
