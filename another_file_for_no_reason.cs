e5d9f68f89ca2144bb929eab1a2ec23643eaf405
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-22 22:54:09 -0500

    Create another_file_for_no_reason.cs

---
+++ another_file_for_no_reason.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Genric;
+
+// This is me just practicing what i've learned
+/*
+If you're interested then please, be my guest and read my newby ass c# code!
+If you'r not then why the hell did you even click on the project the clearly states "not a project"
+Lol, I'm just messing with you. Enjoy reading my newby ass code
+*/
+
+namespace IWantToGetData
+{
