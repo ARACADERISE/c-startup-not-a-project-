b03f3def5b1867e9b2e3e2a088f318a4a092f474
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-23 12:34:39 -0500

    Update _another_one.java

--- _another_one.java
+++ _another_one.java
@@ -2,6 +2,8 @@ import pract.pract;
 
 public class _another_one extends pract {
   public _another_one() {
+    
+    // NAME is assigned protected so I can assign it a value without having to say this or pass a argument
     NAME = "Aidan";
   }
   public static void main(String[] args) {
