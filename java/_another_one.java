d9160ec571e2014412843e70160abaea63f81cab
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-23 22:25:31 -0500

    Update _another_one.java

--- java/_another_one.java
+++ java/_another_one.java
@@ -1,6 +1,7 @@
 import pract.pract;
+import abstractClasses.abstractClasses;
 
-public class _another_one extends pract {
+public class _another_one extends pract implements abstractClasses {
   public _another_one() {
     
     // NAME is assigned protected so I can assign it a value without having to say this or pass a argument
@@ -9,5 +10,15 @@ public class _another_one extends pract {
   public static void main(String[] args) {
     _another_one name_is = new _another_one();
     name_is.returnName();
+    
+    // Now we use that abstracted class
+    abstractClasses assign = new abstractClasses() {
+      @Override public String aSubClass(String n) {
+        this.name = n;
+        System.out.println("Heyo " + this.name.toUpperCase());
+        return "Done";
+      };
+      assign.aSubClass("Aidan");
+    }
   }
 }
