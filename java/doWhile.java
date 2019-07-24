f7adeef9a9d16943c2b6eaa8ee064ba58f8afc4c
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-23 22:34:38 -0500

    Update doWhile.java

--- java/doWhile.java
+++ java/doWhile.java
@@ -1,5 +1,17 @@
 package doWhile;
 
 public class doWhile {
+  public void doTheDo(int theNumberWeAreDoing) {
+    do {
+      System.out.println("Number " + theNumberWeAreDoing + " is not dead yet");
+      if(theNumberWeAreDoing == 400) {
+        System.out.println("The number " + theNumberWeAreDoing + " is dead");
+      }
+      theNumberWeAreDoing+=10;
+    } while(theNumberWeAreDoing < 400);
+  }
   
+  public static void main(String[] args) {
+    doTheDo numberWillBe = new doTheDo(250); 
+  }
 }
