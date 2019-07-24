a2252c39db3437e6703a2e44df70822fbbd07491
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-23 22:50:34 -0500

    Update doWhile.cs

--- c#/doWhile.cs
+++ c#/doWhile.cs
@@ -1,4 +1,17 @@
 using System;
 using System.Collection.Generic;
 
-namespace
+namespace somethingToDoWithADoWhile
+{
+  class doWhile
+  {
+    public static void Main(string[] args)
+    {
+      int a = 50;
+      do {
+        Console.WriteLine(a);
+        a+=10;
+      } while(a < 500);
+    }
+  }
+}
