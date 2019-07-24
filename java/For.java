31f9fdc54bce309a24e9b4808c763d37545df3f0
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-23 22:48:34 -0500

    Update For.java

--- java/For.java
+++ java/For.java
@@ -1,3 +1,23 @@
 package For;
 
-public class For{}
+public class For {
+  public void doTheF(int a) {
+    //One way of writing a for loop
+    for(int b = 0; b < a; b ++) {
+      System.out.println(b);
+    }
+    //Here is another way
+    for(;a < 100;a+=2) {
+      System.out.println(a);
+    }
+    //The third way
+    for(;a<100;) {
+      System.out.println(a);
+      a+=4
+    }
+  }
+  
+  public static void main(String[] args) {
+    doTheF num = new doTheF(10);
+  }
+}
