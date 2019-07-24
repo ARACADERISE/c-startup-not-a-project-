2ea8a4256b27deb25e207c130ebadcc86889dc49
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-23 22:40:03 -0500

    Update switch.java

--- java/switch.java
+++ java/switch.java
@@ -1,5 +1,31 @@
 package switch;
 
 public class switch {
-
+  public void doTheS(int aNum) {
+    while(aNum < 10) {
+      switch(aNum){
+        case 5:
+          System.out.println("It's " + aNum);
+          while(aNum < 50) {
+            System.out.println(aNum);
+            aNum+=2;
+          }
+          break;
+        case 10:
+          System.out.println("It's " + aNum);
+          while(aNum < 100) {
+            System.out.println(aNum);
+            aNum+=4;
+          }
+          break
+        default:
+          System.out.println("It's " + aNum);
+          while(aNum < 150) {
+            System.out.println(aNum);
+            aNum+=8;
+          }
+          break
+      }
+    }
+  }
 }
