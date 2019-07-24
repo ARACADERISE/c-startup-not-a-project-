2c168a89f593a52ac8b00d88c2a8dc2fcc086239
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-23 23:13:22 -0500

    Update Switch.cs

--- c#/Switch.cs
+++ c#/Switch.cs
@@ -1,2 +1,38 @@
 using System;
 using System.Collections.Generic;
+
+namespace doingTheSwitch
+{
+  class Switch
+  {
+    public static void Main(string[] args) 
+    {
+      int a = 10;
+      while(a < 25){
+       switch(a){
+         case 5:
+           Console.WriteLine($"a is @ {a}");
+           while(a < 100) {
+             Console.WriteLine(a);
+             a+=2;
+           }
+           break;
+         case 10:
+           Console.WriteLine($"a is @ {a}");
+           while(a < 150) {
+             Console.WriteLine(a);
+             a+=4;
+           }
+           break;
+         default:
+           Console.WriteLine($"a is @ {a}");
+           while(a < 200) {
+             Console.WriteLine(a);
+             a+=8;
+           }
+           break;
+       }
+      }
+    }
+  }
+}
