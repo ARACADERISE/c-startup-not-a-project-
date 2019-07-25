4eec583661ea9cc361bad778f8fa48e93d1472f5
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-07-24 21:59:13 -0500

    Update enuM.java

--- java/enuM.java
+++ java/enuM.java
@@ -1,1 +1,26 @@
+package enuM;
 
+public class enuM {
+  enum NAME {
+    NAME_TYPE,
+    NAME_DATA
+  }
+  
+  public void getName extends NAME {
+    public getName() {
+      enuM en = new enuM.NAME_TYPE;
+      
+      switch(en) {
+        case NAME_TYPE:
+          System.out.println("String");
+          break;
+        case NAME_DATA:
+          System.out.println("ARACADERISE");
+      }
+    }
+  }
+  
+  public static void main(String[] args) {
+    getName name = new getName(); 
+  }
+}
